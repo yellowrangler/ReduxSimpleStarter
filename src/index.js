246e737af6aec76ca72bf564028fbc4c96594de2
tarryc <tarryc@manchester.westell.com>
2017-06-18 17:10:33 -0400

    final video with react

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 // google browser API key
 const API_KEY = 'AIzaSyDLoojC0Yhmx-DMJ0f-XPayu7dykj8kbQ8';
@@ -12,22 +14,36 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch( {key: API_KEY, term: 'surfboards'},(videos)  => {
-      this.setState({ videos });
-      // this.setState({ videos: videos }); if both key and property are the same
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch( {key: API_KEY, term: term},(videos)  => {
+      this.setState({ 
+        videos,
+        selectedVideo: videos[0]
+       });
     });
   }
+
   render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoList videos={this.state.videos}/>
+        <SearchBar onVideoSearchTermChange= {videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} /> 
+        <VideoList 
+          onVideoSelect= {selectedVideo => this.setState({selectedVideo}) }
+          videos={this.state.videos} />
       </div>
     );
-  }
-
+  };
 }
 
 // Takes this components generated HTML and put it on the page
